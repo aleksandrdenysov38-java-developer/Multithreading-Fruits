f14b9fadc143ee13b565d19143b05c935c8d89e8
alex <aleksandrdenysov38@gmail.com>
2026-02-28 22:19:15 +0200

    Ready project

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,14 @@
 package app;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("This is my app");
+        DataHandler dataHandler = new DataHandler();
+        MyThread myThread1 = new MyThread("Thread 1", dataHandler);
+        MyThread myThread2 = new MyThread("Thread 2", dataHandler);
+        myThread1.start();
+        myThread2.start();
     }
 }
+
+
